fbd99d50e8072371d62092f1fd68400b260ec7b7
ojdarez <ojdarez@gmail.com>
2021-08-03 03:15:30 -0400

    Completed redirect process after client authorization.
    Redirect page fixed from Zendesk auth code api  specified redirect process to .../home.

--- app.js
+++ app.js
@@ -1,23 +1,138 @@
-//imports
-const express = require('express')
+//imPORTs
+const Tickets = require('./public/js/getTickets')
+const cp = require('child_process')
+const ls = require('node-localstorage');
+const express = require('express');
+const { URL } = require("url");
 const path = require('path');
+const http = require("http");
+const open = require("open");
+
+var isAuthorized = false;
+const PORT = 3000
 const app = express()
-const port = process.env.PORT || 3000
+const C_ID = "test_tickers"
+const SUBDOMAIN = "zccojdarez"
+const SCOPE = "tickets:read write"
+const TICKET_URL_1 = `https://${SUBDOMAIN}.zendesk.com/api/v2/tickets.json?per_page=25`;
+const REDIRECT_URL = 'http://localhost:3000/' //'https://ojdarez.github.io/zendesk-c3/' 
+const z_AUTH_ROOT_URL = new URL(`https://${SUBDOMAIN}.zendesk.com/oauth/authorizations/new`);
 
-//Static files...
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
+if (typeof localStorage === "undefined" || localStorage === null) {
+    const LocalStorage = ls.LocalStorage;
+    var localStorage = new LocalStorage('./scratch');
+}
+
+app.use(express.urlencoded({extended: false}))
 
 
-//Set Views
-app.set('views', './views')
+function zAuthUrl() {
+    z_AUTH_ROOT_URL.search = new URLSearchParams({
+        response_type: "code",
+        rediect_uri: REDIRECT_URL,
+        client_id: C_ID,
+        scope: SCOPE,
+    })
 
-app.get('', (req, res) => {
-    res.render('\index')
+    const params = z_AUTH_ROOT_URL.search
+    return `${z_AUTH_ROOT_URL}${params}`
+}
+
+async function getAuthorization() {
+    const zAuthCodeUrl = zAuthUrl();
+
+    const server = http.createServer( async (req, res) => {
+        
+        var redirUrl = req.url
+        console.log(redirUrl)
+        var authCodeUrl = new URL(`${REDIRECT_URL}` + redirUrl)
+        var isCode = authCodeUrl.searchParams.get('code')
+        
+        if (res.statusCode == 200 && isCode != null) {
+            //UPDATE LOCAL STORAGE DATA
+            if(localStorage['code']) {localStorage.removeItem('code')}
+            localStorage.setItem('code', isCode)
+            //CLOSE SERVER AND NOTIFY USER OF SUCCESS
+            isAuthorized = true
+        } else {
+            var isErr = authCodeUrl.searchParams.get('error')
+            console.log(isErr)
+            isAuthorized = false
+        } 
+
+        res.end(`<h1>Welcome to Zendesk Tickers</h1>
+            <a id="pageAnchor" class="btn" href="home/tickets">
+                <img src="https://media.giphy.com/media/ka6loLNrqm0ao7LLbl/giphy.gif" alt="this slowpoke moves" width="250" />
+            </a>
+        `);
+        req.socket.end();
+        req.socket.destroy();
+        server.close()
+
+        //ONCE AUTH CONNECTION IS CLOSED: DO THIS
+        if (res.writableFinished) {
+            //RESTART SERVER FOR APP CONNECTION
+            app.listen(PORT, () => console.info(`Let's GO! \nTickers on port: ${PORT} here to serve you`))
+            eventsEmitter.emit('clientAuthorized', redirUrl)
+
+            //LOAD TICKETS JSON
+            const myTickets = new Tickets()
+            await myTickets.getTickets(TICKET_URL_1)
+        } else {
+            console.error('The connection was terminated while the message was still being sent');
+        }
+    }).listen(PORT, () => console.log('Listening for authorization on PORT ' + PORT)).on("error", err => {
+        console.log("Error: " + err.message);
+        return err.statusCode;
+    });
+    if (!isAuthorized) {await open(zAuthCodeUrl)}
+}
+
+const EventEmitter = require('events');
+const eventsEmitter = new EventEmitter()
+
+eventsEmitter.on('clientAuthorized', async sentTo => {    
+
+    app.get('/', (req, res) => {
+        isAuthorized ? res.redirect('/home') : res.redirect('/login')
+    })
+
+    app.get(`/home`, (req, res) => {
+        res.send(`<h1>Welcome to Zendesk Tickers</h1>
+            <a id="pageAnchor" class="btn" href="home/tickets">
+                <img src="https://media.giphy.com/media/ka6loLNrqm0ao7LLbl/giphy.gif" alt="this slowpoke moves" width="250" />
+            </a>
+        `)
+    })
+    
+    app.get("/home/tickets", (req, res) => {
+        //if getAuth != error
+        //TODO
+        //else
+        //TODO 
+        res.send("Display number of tickets!")
+    })
+    
+    app.get('/login', (req, res) => {
+        //getAuthorization()
+        //TODO
+        //Gif Display.
+        //Display click to view tickets.
+    })
+
+    app.use(function(req, res, next) {
+        if (req.originalUrl == sentTo) {
+            res.redirect('/home')
+        } else {
+            res.status(404).send(`
+            <img src="https://cdn.glitch.com/0e4d1ff3-5897-47c5-9711-d026c01539b8%2Fbddfd6e4434f42662b009295c9bab86e.gif?v=1573157191712" width="404" />
+            <a href="/home><span>Sorry, that route doesn't exist :)</span></a>
+            `)
+        }
+    })
+
 })
 
-//C:\Users\JOJ_MSFT\Documents\Code\ZENDESK_PROJ\views\index.html
-
-//Listen on port 3000
-app.listen(port, () => console.info(`Let's GO!\nListening on port ${port}`))
+getAuthorization().then(()=> {
+    eventsEmitter.emit('clientAuthorized')
+})
--- public/js/index.js
+++ public/js/index.js
@@ -1,67 +1,25 @@
-"use strict";
+//"use strict";
+const ls = require('node-localstorage');
 const { URL } = require("url");
 const axios = require("axios");
 const http = require("http");
 const open = require("open");
-var fs = require('fs');
+const fs = require('fs');
 
 const C_ID = "test_tickers"
 const SUBDOMAIN = "zccojdarez"
 const SCOPE = "tickets:read write"
 const PORT = process.env.port || 3000;
 const REDIRECT_URL = 'http://localhost:3000/' //'https://ojdarez.github.io/zendesk-c3/'
-const TOKEN_URL = `https://${SUBDOMAIN}.zendesk.com/oauth/tokens`
-const TICKET_URL = `https://${SUBDOMAIN}.zendesk.com/api/v2/tickets.json`; 
+const TOKEN_URL = `https://${SUBDOMAIN}.zendesk.com/oauth/tokens` 
 const z_AUTH_ROOT_URL = new URL(`https://zccojdarez.zendesk.com/oauth/authorizations/new`);
 
 if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require('node-localstorage').LocalStorage;
+    const LocalStorage = ls.LocalStorage;
     var localStorage = new LocalStorage('./scratch');
 }
 
-function zAuthUrl() {
-    z_AUTH_ROOT_URL.search = new URLSearchParams({
-        response_type: "code",
-        rediect_uri: REDIRECT_URL,
-        client_id: C_ID,
-        scope: SCOPE,
-    })
-
-    const params = z_AUTH_ROOT_URL.search
-    return `${z_AUTH_ROOT_URL}${params}`
-}
-
-async function getAuthorization() {
-    const zAuthCodeUrl = zAuthUrl();
-
-    const server = http.createServer( async (req, res) => {
-    
-        var authCodeUrl = new URL(`${REDIRECT_URL}` + req.url)
-        var isCode = authCodeUrl.searchParams.get('code')
-
-        if (res.statusCode == 200 && isCode != null) {
-            if(localStorage['code']) {localStorage.removeItem('code')}
-            localStorage.setItem('code', isCode)
-            
-        } else {
-            var isErr = authCodeUrl.searchParams.get('error')
-            console.log(isErr)
-        }
-            
-
-        res.end("They!");
-        req.socket.end();
-        req.socket.destroy();
-        server.close()
-    }).listen(PORT, () => console.log('Listening for code on port ' + PORT)).on("error", err => {
-        console.log("Error: " + err.message);
-        return err.statusCode;
-    });
-
-    open(zAuthCodeUrl)
-}
-
-getAuthorization()
+module.exports = {Auth, isAuthorized};
 
 // async function setSecret() {
 //     try {
